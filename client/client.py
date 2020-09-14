9079dd967e337afe577a572d6214f363b21f97c4
Adam <aquinn@andrew.cmu.edu>
2020-09-14 10:32:00 -0400

    Removed env directory and added .py files for client & server.

---
+++ client/client.py
@@ -0,0 +1,7 @@
+#
+#    File: Client Application
+#  Author: aquinn, nsaizan, ranz2
+#   Group: TheEnemy'sGateIsDown
+#    Date: 9/14/2020
+#
+# Main code for our client.
---
+++ server/server.py
@@ -0,0 +1,16 @@
+#
+#    File: Server Application
+#  Author: aquinn, nsaizan, ranz2
+#   Group: TheEnemy'sGateIsDown
+#    Date: 9/14/2020
+#
+# Main code for our server.
+
+import socket
+
+HOST = ''    #Accept connections on any IPv4 interface
+PORT = 36337
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    pass
