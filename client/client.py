2c700509f38d9b4d67e494adaef8cdac434a5e75
nsaizan <nsaizan@andrew.cmu.edu>
2020-10-10 16:26:37 -0400

    Put client code in an if __name__==__main__ block

--- client/client.py
+++ client/client.py
@@ -21,61 +21,54 @@ from ports  import ports
 # HOST & PORT SETTINGS  #
 # # # # # # # # # # # # #
 
-#HOST = ''    # Accept connections on any IPv4 interface
-#PORT = 36337
+if __name__ == '__main__':
 
-# # # # # 
-# MAIN  #
-# # # # #
-# Parse client number from CLI
-if len(sys.argv) < 2:
-    raise ValueError("No Client Number Provided!")
- 
-if len(sys.argv) > 2:
-    raise ValueError("Too Many CLI Arguments!")
- 
-client_num = int(sys.argv[1])
+    # Parse client number from CLI
+    if len(sys.argv) < 2:
+        raise ValueError("No Client Number Provided!")
+    
+    if len(sys.argv) > 2:
+        raise ValueError("Too Many CLI Arguments!")
+    
+    client_num = int(sys.argv[1])
 
-request_num = 0
+    request_num = 0
 
-# Open the connection to the server.
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Open the connection to the server.
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
-    # Connect to the server
-    s.connect((HOST, ports["S1_LISTENING"]))
+        # Connect to the server
+        s.connect((HOST, ports["S1_LISTENING"]))
 
-    # Setup logger
-    logger = Logger()
-    messenger = Messenger(s, f'C{client_num}', 'S1', logger)
+        # Setup logger
+        logger = Logger()
+        messenger = Messenger(s, f'C{client_num}', 'S1', logger)
 
-    message_count = 0
+        message_count = 0
 
-    # Run until client closes the connection
-    while(True):
-        # Get user input
-        attack_value = input("What is your next attack? ")
+        # Run until client closes the connection
+        while(True):
+            # Get user input
+            attack_value = input("What is your next attack? ")
 
-        # Check if user requested to exit
-        if(attack_value in ['exit','close','quit']):
-            print("INFO: Closing connection")
-            s.close()
-            break
+            # Check if user requested to exit
+            if(attack_value in ['exit','close','quit']):
+                print("INFO: Closing connection")
+                s.close()
+                break
 
-        # Check user input format
-        if(attack_value.isdecimal() == False):
-            print("ERROR: Input must be a valid integer")
-            continue
+            # Check user input format
+            if(attack_value.isdecimal() == False):
+                print("ERROR: Input must be a valid integer")
+                continue
+                
+            # Send and log the attack
+            request_num = request_num + 1
+            messenger.send(f"(Req#{request_num}) {attack_value}")
+
+            # Wait to receive a reply (will be logged automatically) 
+            while(not (messenger.recv(s, 1024))):
+                pass
             
-        # Send and log the attack
-        request_num = request_num + 1
-        messenger.send(f"(Req#{request_num}) {attack_value}")
-
-        # Wait to receive a reply (will be logged automatically) 
-        while(not (messenger.recv(s, 1024))):
-            pass
-        
-        
-    s.close()
-
-        
-        
+            
+        s.close()
