e240e2d4323d2d6990361d6a6c18291586af4d83
nsaizan <nsaizan@andrew.cmu.edu>
2020-10-10 16:24:27 -0400

    Updated LFD to request membership changes from GFD

--- detector/detector.py
+++ detector/detector.py
@@ -35,18 +35,19 @@ NORMALIZED_TIMEOUT = 5
 MAX_MSG_LEN = 1024 #Max number of bytes we're willing to receive at once.
 
 #Determined at runtime
-MY_NAME = "" 
-MY_SERVER = ""
+MY_NAME = None
+MY_SERVER = None
 
 #Logger instance for the LFD
-lfd_logger = Logger()
-hb_messenger = Messenger(None, MY_NAME, MY_SERVER, lfd_logger)
-gfd_messenger = Messenger(None, MY_NAME, "GFD"    , lfd_logger)
+lfd_logger = None
+hb_messenger = None
+gfd_messenger = None
 
 #Macro to get a timestamp in ms.
 get_time = lambda: int(round(time.time() * 1000))
 
-hb_sent_num = 0;
+hb_sent_num = 0
+
 
 # # # # # # # # # # # # #
 # THREAD FUNCTION DEFNS #
@@ -55,6 +56,8 @@ hb_sent_num = 0;
 # Continuously wait to receive receipts on a specified connection.
 # If no receipt is received before the timeout, throw an error and exit.
 def get_receipts(frequency, hb_conn):
+    #The status of this LFD's server
+    server_is_initialized = False
 
     timeout = 1000 * NORMALIZED_TIMEOUT / frequency
     last_receipt_time = get_time()
@@ -62,17 +65,22 @@ def get_receipts(frequency, hb_conn):
     while True:
 
         try:
-            if get_time() - last_receipt_time > timeout:
+            if (get_time() - last_receipt_time) > timeout:
                 lfd_logger.error("AHHHH! THE SERVER IS DEAD!")
+                gfd_messenger.send(f"{MY_NAME}: delete replica {MY_SERVER}")
                 hb_conn.close()
                 return
 
             # Wait for the server to send back receipt
-            x = hb_messenger.recv(hb_conn,25)
+            x = hb_messenger.recv(hb_conn)
 
             #If we receive a heartbeat back from the server.
             if(x):
                 last_receipt_time = get_time()
+                # If this is the server's first hb then we tell the GFD
+                if(server_is_initialized == False):
+                    gfd_messenger.send(f"{MY_NAME}: add replica {MY_SERVER}")
+                    server_is_initialized = True
 
         except Exception as e:
             lfd_logger.warning("Error while getting receipt! (Connection may have closed.)")
@@ -100,76 +108,82 @@ def serve_GFD(gfd_conn):
       
     try:
         while(1):
-            # Wait for the client to send a heartbeat and echo it back.
-            msg = gfd_messenger.recv(gfd_conn, MAX_MSG_LEN)
+            # Wait for the GFD to send a heartbeat and echo it back.
+            msg = gfd_messenger.recv(gfd_conn)
             if msg:
                 gfd_messenger.send(msg)     
 
     except KeyboardInterrupt:
         print("Killing GFD heartbeat thread.")
         exit()
- 
-# # # # #
-# MAIN  #
-# # # # #
-# Parse heartbeat frequency from the input
-if len(sys.argv) < 2:
-    raise ValueError("No Replica Number Provided!")
 
-if len(sys.argv) < 3:
-    raise ValueError("No Heartbeat Frequency Provided!")
- 
-if len(sys.argv) > 3:
-    raise ValueError("Too Many CLI Arguments!")
+def main():
+    #Once in the beginning we connect to the GFD. The GFD is a single point of
+    #failure, so we don't anticipate ever having to re-connect to it.
+    gfd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-replica_num = int(sys.argv[1])
-freq = float(sys.argv[2])
-
-MY_NAME = "LFD" + str(replica_num)
-MY_SERVER = "S" + str(replica_num)
-
-
-
-
-#Once in the beginning we connect to the GFD. The GFD is a single point of
-#failure, so we don't anticipate ever having to re-connect to it.
-gfd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-while(gfd_socket.connect_ex((HOST, ports["GFD_LISTEN"]))):
-    lfd_logger.warning("GFD not available.")
-    time.sleep(1)
-
-lfd_logger.info('Registered with the GFD.')
-gfd_messenger.socket = gfd_socket
-
-gfd_heartbeat = threading.Thread(target=serve_GFD, args = (gfd_socket,))
-gfd_heartbeat.start()
-
-while True:
-
-
-    hb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Connect to the server on the HB socket.
-    while(hb_socket.connect_ex((HOST, ports[MY_SERVER+"_HB"]))):
-        lfd_logger.warning("HB port not available.")
+    while(gfd_socket.connect_ex((HOST, ports["GFD_LISTEN"]))):
+        lfd_logger.warning("GFD not available.")
         time.sleep(1)
 
-    lfd_logger.info('HB connection established')
-    hb_messenger.socket = hb_socket
+    lfd_logger.info('Registered with the GFD.')
+    gfd_messenger.socket = gfd_socket
+
+    # Identify ourself to the GFD
+    gfd_messenger.send(MY_NAME)
+
+    gfd_heartbeat = threading.Thread(target=serve_GFD, args = (gfd_socket,))
+    gfd_heartbeat.start()
+
+    while True:
+
+
+        hb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect to the server on the HB socket.
+        while(hb_socket.connect_ex((HOST, ports[MY_SERVER+"_HB"]))):
+            lfd_logger.warning("HB port not available.")
+            time.sleep(1)
+
+        lfd_logger.info('HB connection established')
+        hb_messenger.socket = hb_socket
+        
+        #Set up function to continuously send heartbeats.
+        heartbeat = threading.Thread(target=send_heartbeat, args = (freq, hb_socket))
+        heartbeat.start()
+
+        receipts = threading.Thread(target=get_receipts, args=(freq, hb_socket))
+        receipts.start()
+
+        #Wait for  the two perpetual threads to die before starting the process
+        #over again.
+        receipts.join()
+        heartbeat.join()
+
+        hb_socket.close()
+
+if __name__ == '__main__':
+
+    # Parse heartbeat frequency from the input
+    if len(sys.argv) < 2:
+        raise ValueError("No Replica Number Provided!")
+
+    if len(sys.argv) < 3:
+        raise ValueError("No Heartbeat Frequency Provided!")
     
-    #Set up function to continuously send heartbeats.
-    heartbeat = threading.Thread(target=send_heartbeat, args = (freq,hb_socket))
-    heartbeat.start()
+    if len(sys.argv) > 3:
+        raise ValueError("Too Many CLI Arguments!")
 
-    receipts = threading.Thread(target=get_receipts, args=(freq,hb_socket))
-    receipts.start()
+    replica_num = int(sys.argv[1])
+    freq = float(sys.argv[2])
 
-    #Wait for  the two perpetual threads to die before starting the process
-    #over again.
-    receipts.join()
-    heartbeat.join()
+    MY_NAME = "LFD" + str(replica_num)
+    MY_SERVER = "S" + str(replica_num)
 
-    hb_socket.close()
+    #Logger instance for the LFD
+    lfd_logger = Logger()
+    hb_messenger = Messenger(None, MY_NAME, MY_SERVER, lfd_logger)
+    gfd_messenger = Messenger(None, MY_NAME, "GFD", lfd_logger)
 
+    main()
 
