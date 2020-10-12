cacc70bcc7e6d275a69418d62cae4a3cd7cb151a
RanZhang-CR <59900405+RanZhang-CR@users.noreply.github.com>
2020-10-12 16:22:53 -0400

    serve duplication detection

--- client/client.py
+++ client/client.py
@@ -9,6 +9,7 @@
 import socket
 import time
 import sys
+import threading
 sys.path.append("..")
 
 # Custom imports
@@ -21,7 +22,17 @@ from ports  import ports
 # HOST & PORT SETTINGS  #
 # # # # # # # # # # # # #
 
+# # # # # # # # # # # # #
+# # GLOBAL VARIABLES  # #
+# # # # # # # # # # # # #
+to_be_receive = []
+
+
 if __name__ == '__main__':
+    # Flag to mark whether server is available
+    s1_alive = 1
+    s2_alive = 1
+    s3_alive = 1
 
     # Parse client number from CLI
     if len(sys.argv) < 2:
@@ -29,46 +40,133 @@ if __name__ == '__main__':
     
     if len(sys.argv) > 2:
         raise ValueError("Too Many CLI Arguments!")
-    
-    client_num = int(sys.argv[1])
 
+    client_num = int(sys.argv[1])
     request_num = 0
 
     # Open the connection to the server.
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Connect to the server
-        s.connect((HOST, ports["S1_LISTENING"]))
+    # Connect to the server
+    try:
+        s1.connect((HOST, ports[f"S1_LISTEN"]))
+    except Exception as e:
+        s1_alive = 0
+    try:
+        s2.connect((HOST, ports[f"S2_LISTEN"]))
+    except Exception as e:
+        s2_alive = 0
+    try:
+        s3.connect((HOST, ports[f"S3_LISTEN"]))
+    except Exception as e:
+        s3_alive = 0
 
-        # Setup logger
-        logger = Logger()
-        messenger = Messenger(s, f'C{client_num}', 'S1', logger)
+    # Setup logger
+    logger = Logger()
+    if s1_alive:
+        messenger1 = Messenger(s1, f'C{client_num}', 'S1', logger)
+    if s2_alive:
+        messenger2 = Messenger(s2, f'C{client_num}', 'S2', logger)
+    if s3_alive:
+        messenger3 = Messenger(s3, f'C{client_num}', 'S3', logger)
 
-        message_count = 0
+    # Run until client closes the connection
+    while (True):
+        # Get user input
+        attack_value = input("What is your next attack? ")
 
-        # Run until client closes the connection
-        while(True):
-            # Get user input
-            attack_value = input("What is your next attack? ")
+        # Check if user requested to exit
+        if (attack_value in ['exit', 'close', 'quit']):
+            print("INFO: Closing connection")
+            if s1_alive:
+                s1.close()
+            if s2_alive:
+                s2.close()
+            if s3_alive:
+                s3.close()
+            break
 
-            # Check if user requested to exit
-            if(attack_value in ['exit','close','quit']):
-                print("INFO: Closing connection")
-                s.close()
-                break
+        # Check user input format
+        if (attack_value.isdecimal() == False):
+            print("ERROR: Input must be a valid integer")
+            continue
 
-            # Check user input format
-            if(attack_value.isdecimal() == False):
-                print("ERROR: Input must be a valid integer")
-                continue
-                
-            # Send and log the attack
-            request_num = request_num + 1
-            messenger.send(f"(Req#{request_num}) {attack_value}")
-
-            # Wait to receive a reply (will be logged automatically) 
-            while(not (messenger.recv(s, 1024))):
+        # Send and log the attack
+        request_num = request_num + 1
+        to_be_receive.append(request_num)
+        if s1_alive:
+            try:
+                messenger1.send(f"(Req#{request_num}) {attack_value}")
+            except Exception as e:
+                s1_alive = 0
                 pass
-            
-            
-        s.close()
+        if s2_alive:
+            try:
+                messenger2.send(f"(Req#{request_num}) {attack_value}")
+            except Exception as e:
+                s2_alive = 0
+                pass
+        if s3_alive:
+            try:
+                messenger3.send(f"(Req#{request_num}) {attack_value}")
+            except Exception as e:
+                s3_alive = 0
+                pass
+
+        # Wait to receive a reply (will be logged automatically)
+        # while (not (messenger.recv(s))):
+        # pass
+
+        if s1_alive:
+            try:
+                msg1 = messenger1.recv(s1)
+                if msg1:
+                    if request_num in to_be_receive:
+                        to_be_receive.remove(request_num)
+                    else:
+                        print("discard duplicated info.")
+                else:
+                    continue
+            except Exception as e:
+                s1_alive = 0
+                pass
+
+        if s2_alive:
+            try:
+                msg2 = messenger2.recv(s2)
+                if msg2:
+                    if request_num in to_be_receive:
+                        to_be_receive.remove(request_num)
+                    else:
+                        print("discard duplicated info.")
+                else:
+                    continue
+            except Exception as e:
+                s2_alive = 0
+                pass
+
+        if s3_alive:
+            try:
+                msg3 = messenger3.recv(s3)
+                if msg2:
+                    if request_num in to_be_receive:
+                        to_be_receive.remove(request_num)
+                    else:
+                        print("discard duplicated info.")
+                else:
+                    continue
+            except Exception as e:
+                s3_alive = 0
+                pass
+
+    if s1_alive:
+        s1.close()
+    if s2_alive:
+        s2.close()
+    if s3_alive:
+        s3.close()
+
+
+
