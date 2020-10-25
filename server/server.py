e60cdc88fefa97616d91d9ed5057eb650dfd268f
Nick Saizan <nsaizan@andrew.cmu.edu>
2020-10-25 14:55:09 -0400

    Added checkpointing capabilities to server replicas

--- server/server.py
+++ server/server.py
@@ -8,6 +8,7 @@
 
 # Standard imports
 import socket
+import _thread
 import threading
 import sys
 import time
@@ -22,17 +23,142 @@ from helper import Messenger
 from ports  import ports
 from ports  import HOST
 
-MY_NAME = "" #Determined at runtime.
+MY_NAME = "" # Determined at runtime.
 
-MAX_MSG_LEN = 1024 #Max number of bytes we're willing to receive at once.
+MAX_MSG_LEN = 1024 # Max number of bytes we're willing to receive at once.
 
-SERVE_CLIENT_PERIOD = 0.01 #seconds
+SERVE_CLIENT_PERIOD = 0.01 # seconds
+
+SEND_CP_PERIOD = 5 # seconds
+
+backup1, backup2 = None, None
+backup1_alive, backup2_alive = False, False
+backup1_messenger, backup2_messenger = None, None
+
+cp_num = 0 # Checkpoint number
+last_cp_time = None # Last time we sent a checkpoint
 
 # # # # # # # # # # # # #
 # THREAD FUNCTION DEFNS #
 # # # # # # # # # # # # #
+
+def we_are_primary():
+    return replica_num == 1
+
+def wait_for_clients():
+    while(not clients):
+        pass
+
+def parse_client_message(logger, msg):
+    attack = None
+    req_num = None
+    ret_val = False
+
+    if msg:
+        if "Req" in msg:
+            try: 
+                attack = int(msg.split(')')[1])
+                req_num = int(msg.split(')')[0].split('#')[1])
+                ret_val = True
+            except (IndexError, ValueError):
+                logger.error('Bad message from client.')
+
+    return attack, req_num, ret_val
+
+def parse_checkpoint_message(logger, msg):
+    state = None
+    cp_num = None
+    ret_val = False
+
+    if msg:
+        if "Checkpoint" in msg:
+            try: 
+                state = int(msg.split(')')[1])
+                cp_num = int(msg.split(')')[0].split('#')[1])
+                ret_val = True
+            except (IndexError, ValueError):
+                logger.error('Bad message from client.')
+
+    return state, cp_num, ret_val
+
+def repair_connection(logger, port_name, destination):
+    new_server = None
+    new_status = False
+    new_messenger = None
+    try:
+        new_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        new_server.connect((HOST, ports[port_name]))
+        new_messenger = Messenger(new_server, MY_NAME, destination, logger)
+        new_status = True
+    except Exception:
+        new_status = False
+
+    return new_server, new_status, new_messenger
+
+def repair_connections(logger, backups, statuses, messengers):
+    if statuses[0] == False:
+        new_connection = repair_connection(logger, f"S2_LISTEN", "S2")
+        backups[0], statuses[0], messengers[0] = new_connection
+
+    if statuses[1] == False:
+        new_connection = repair_connection(logger, f"S3_LISTEN", "S3")
+        backups[1], statuses[1], messengers[1] = new_connection
+
+    return backups, statuses, messengers
+
+def its_time_to_send_cp():
+    global last_cp_time
+
+    current_time = time.time()
+
+    if last_cp_time == None:
+        last_cp_time = current_time
+        return True
+
+    if (current_time - last_cp_time) > SEND_CP_PERIOD:
+        last_cp_time = current_time
+        return True
+
+    return False
+
+def send_checkpoint(logger, num_enemies):
+    global backup1, backup2
+    global backup1_alive, backup2_alive
+    global backup1_messenger, backup2_messenger
+
+    global cp_num
+
+    # Ensure we are the primary replica
+    if not we_are_primary():
+        return
+
+    # Repair connections if needed
+    backups = [backup1, backup2]
+    statuses = [backup1_alive, backup2_alive]
+    messengers = [backup1_messenger, backup2_messenger]
+
+    val = repair_connections(logger, backups, statuses, messengers)
+    backups, statuses, messengers = val
+
+    backup1, backup2 = backups
+    backup1_alive, backup2_alive = statuses
+    backup1_messenger, backup2_messenger = messengers
+
+    if backup1_alive:
+        try:
+            backup1_messenger.send(f"(Checkpoint#{cp_num}) {num_enemies}")
+        except Exception:
+            backup1_alive = False
+    if backup2_alive:
+        try:
+            backup2_messenger.send(f"(Checkpoint#{cp_num}) {num_enemies}")
+        except Exception:
+            backup2_alive = False
+
+    cp_num += 1
+
 # Function to serve a single client.
-def serve_clients():
+def serve_clients_and_replicas():
     # Ensure that we are referencing the global num_enemies
     global num_enemies
     global clients_lock
@@ -43,45 +169,53 @@ def serve_clients():
     try: 
         while(1):
 
-            while(not clients):
-                pass
+            wait_for_clients()
 
             #Give the main loop time to get more clients.
             time.sleep(SERVE_CLIENT_PERIOD)
 
             clients_lock.acquire()
-            readable,writable,exceptional = select.select(clients,[],[],0)
+
+            readable = select.select(clients,[],[],0)[0]
 
             for conn in readable:
-
                 # Get the message from the client.
                 msg = messenger.recv(conn)
 
-                if msg:
-                    try: 
-                        attack = int(msg.split(')')[1])
-                        req_num = int(msg.split(')')[0].split('#')[1])
-                    except (IndexError, ValueError) as e:
-                        logger.error('Bad message from client.')
-                        continue
-                else:
-                    continue
+                # Parse checkpoints
+                state, cp_num, is_cp = parse_checkpoint_message(logger, msg)
 
-                # x represents the number of enemies killed.
-                logger.info(f"Before Request: S={num_enemies}")
-                num_enemies = num_enemies - attack
-                logger.info(f"After Request: S={num_enemies}")
+                # Parse client messages
+                attack, req_num, is_client_msg = parse_client_message(logger, msg)
 
-                messenger.socket = conn
-                time.sleep(random.randint(1,3))
-                messenger.send(f"(Req#{req_num}){num_enemies} FORMICS REMAIN")
+                # Process checkpoints
+                if is_cp:
+                    # Process the checkpoint
+                    logger.info(f"Updating internal state to: {state}")
+                    num_enemies = state
+
+                # Process client messages
+                if is_client_msg and we_are_primary():
+                    # Process the attack
+                    logger.info(f"Before Request: S={num_enemies}")
+                    num_enemies = num_enemies - attack
+                    logger.info(f"After Request: S={num_enemies}")
+
+                    # Respond to the attack
+                    messenger.socket = conn
+                    messenger.send(f"(Req#{req_num}){num_enemies} FORMICS REMAIN")
+
+            # Send a checkpoint to backups
+            if its_time_to_send_cp():
+                send_checkpoint(logger, num_enemies)
 
             clients_lock.release()
-    except Exception as e:
+
+    except Exception:
         traceback.print_exc()
         #If something happens to the client-serving part of the server,
         #everything should die.
-        thread.interrupt_main()
+        _thread.interrupt_main()
         return
 
 # Function to receive heartbeats and reply to them.
@@ -124,23 +258,23 @@ def main():
         print("\nAnd remember, this is just a game.")
         print(num_enemies, "FORMICS REMAIN")
 
-        server = threading.Thread(target=serve_clients, args=(), daemon=True)
-        server.start();
+        server = threading.Thread(target=serve_clients_and_replicas, args=(), daemon=True)
+        server.start()
         
-        #Run forever
+        # Run forever
         try:
             while True:
-            
                 # Wait (blocking) for connections.
-                conn, addr = s.accept()
+                conn = s.accept()[0]
 
                 clients_lock.acquire()
                 clients.append(conn)
                 clients_lock.release()
-        except Exception as e: 
+        except Exception:
+            # Gracefully exit by closing s & all connections.
             traceback.print_exc()
             s.shutdown()
-            s.close() #Gracefully exit by closing s & all connections.
+            s.close()
             for conn in clients:
                 conn.shutdown()
                 conn.close()
@@ -166,9 +300,6 @@ if __name__ == '__main__':
     # This starts at 1000 and can be decremented by clients.
     num_enemies = 1000
 
-    # Protect this shared variable with a lock to prevent race conditions.
-    #num_enemies_lock = threading.Lock()
-
     clients = []
 
     clients_lock = threading.Lock()
@@ -178,7 +309,7 @@ if __name__ == '__main__':
     # Before we do anything, make sure the heartbeat is working.
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
-        #Set the socket address so it can be reused without a timeout.
+        # Set the socket address so it can be reused without a timeout.
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         
         # Create the heartbeat port.
@@ -190,11 +321,11 @@ if __name__ == '__main__':
         # Get the HEARTBEAT connection.
         conn, addr = s.accept()
 
-        #Kick off the LFD-server connection.
+        # Kick off the LFD-server connection.
         print("Connection with LFD established!")
 
-        #Make the heartbeat thread a daemon thread so it dies if the rest of
-        #the program goes down.
+        # Make the heartbeat thread a daemon thread so it dies if the rest of
+        # the program goes down.
         heartbeat = threading.Thread(target=serve_LFD, args = (conn,addr), daemon=True)
         heartbeat.start()
 
